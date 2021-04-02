0b5b906a6a4ab65a6714c2f6c35aaea177ecd5b8
Daniel <danielrs9504@gmail.com>
2021-04-02 08:08:16 -0500

    Modifica el layout en el about screen y centra el card

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 
 const AboutScreen = () => {
   return (
     <div id='about'>
-      <div className='about-title'>About me</div>
-      <div className='about-photo'>Foto</div>
+      <div className='about-title'>
+        <h1>About me</h1>
+      </div>
+      <div className='about-photo'>
+        <Card style={{ width: "80%" }}>
+          <Card.Img variant='top' src='holder.js/100px180' />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
       <div className='about-skills'>Skills</div>
       <div className='about-experience'>Experiencia</div>
     </div>
