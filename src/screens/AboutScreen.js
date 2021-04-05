97f0415e7a312cd35cbfe28836dc43f72682e159
Daniel <danielrs9504@gmail.com>
2021-04-05 18:56:44 -0500

    Cambio inicial a bootstrap grid

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -5,28 +5,22 @@ import Skill from "../components/Skill";
 
 const AboutScreen = () => {
   return (
-    <div id='about'>
-      <div className='about-title'>
-        <h1>About me</h1>
+    <div id='about' className='container'>
+      <div className='col-12'>
+        <h1 className='text-center text-uppercase'>About me</h1>
       </div>
-      <div className='about-photo'>
-        <img src={cvImg} className='photo-img' />
-        <div className='photo-text'>
-          I'm a mechanical engineer and fullstack developer, passionate about
-          programing and constant learning
-        </div>
-        {/* <Card style={{ width: "80%" }}>
-          <Card.Img variant='top' src={cvImg} />
-          <Card.Body>
-            <Card.Title>Who am I?</Card.Title>
-            <Card.Text>
+      <div className='col-12'>
+        <div class='card'>
+          <img src={cvImg} class='card-img-top' alt='...' />
+          <div class='card-body'>
+            <p class='card-text'>
               I'm a mechanical engineer and fullstack developer, passionate
               about programing and constant learning
-            </Card.Text>
-          </Card.Body>
-        </Card> */}
+            </p>
+          </div>
+        </div>
       </div>
-      <div className='about-skills'>
+      <div className='col-12'>
         <Skill skill='HTML' width='70' />
         <Skill skill='CSS' width='70' />
         <Skill skill='JavaScript' width='70' />
@@ -34,26 +28,8 @@ const AboutScreen = () => {
         <Skill skill='Node & Express' width='70' />
         <Skill skill='Mongo & Mongoose' width='70' />
         <Skill skill='Ruby & Ruby on Rails' width='70' />
-
-        {/* <div style={{ display: "inline-block" }}>
-          <p>Hola</p>
-          <ProgressBar now={70} />
-        </div> */}
-
-        {/* <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JavaScript</li>
-          <li>React</li>
-          <li>Redux</li>
-          <li>Node</li>
-          <li>Express</li>
-          <li>Mongo & Mongoose</li>
-          <li>Ruby</li>
-          <li>Ruby on Rails</li>
-        </ul> */}
       </div>
-      <div className='about-experience'>Experiencia</div>
+      <div className='col-12'>Experiencia</div>
     </div>
   );
 };
