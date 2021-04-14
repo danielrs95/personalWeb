8281fb7c7b89df04e76b1d3a5f0f48a4cd5fb6ca
Daniel <danielrs9504@gmail.com>
2021-04-14 08:05:18 -0500

    Cambia tema a bootswatch, cambia componentes en el HomeScreen por bootstrap puro

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,8 +2,15 @@ import React from "react";
 
 const HomeScreen = () => {
   return (
-    <div id='home'>
-      <h1 className='home-title'>HomeScreen</h1>
+    <div className='container mt-5'>
+      <div className='row'>
+        <div className='col-12'>
+          <div className='card'>
+            <div className='card-body'> Seccion 1</div>
+            <div className='card-body'>Seccion2</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
