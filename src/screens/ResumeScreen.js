3c6320a4681e1e9a00aa87450e20ee73f1b59192
Daniel <danielrs9504@gmail.com>
2021-07-10 13:12:28 -0500

    1. Crea componente donde estara toda la informacion de la educacion 2. Crea una barra de progreso para mostrar nivel en las habilidades

--- src/screens/ResumeScreen.js
+++ src/screens/ResumeScreen.js
@@ -1,11 +1,12 @@
 import React from "react";
+import EducationComponent from "../components/EducationComponent";
 
 const ResumeScreen = () => {
   return (
     <div className='container card-container'>
       <div className='row justify-content-center'>
         <div className='col-12'>RESUME</div>
-        <div className='col-12'>Education</div>
+        <EducationComponent />
         <div className='col-12'>Experience</div>
         <div className='col-12'>Software Skills</div>
         <div className='col-12'>Coding Skills</div>
