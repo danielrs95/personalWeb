f27ecad3acf88ca8e371a7a0130e06a5c3a8dfe8
Daniel <danielrs9504@gmail.com>
2021-07-10 13:09:12 -0500

    Agrega layout basico del ResumeScreen

--- src/screens/ResumeScreen.js
+++ src/screens/ResumeScreen.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const ResumeScreen = () => {
-  return <div>ResumeScreen</div>;
+  return (
+    <div className='container card-container'>
+      <div className='row justify-content-center'>
+        <div className='col-12'>RESUME</div>
+        <div className='col-12'>Education</div>
+        <div className='col-12'>Experience</div>
+        <div className='col-12'>Software Skills</div>
+        <div className='col-12'>Coding Skills</div>
+      </div>
+    </div>
+  );
 };
 
 export default ResumeScreen;
