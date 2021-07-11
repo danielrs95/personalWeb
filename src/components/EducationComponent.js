d3c5b048fc0129e528a0dbd306448bbe1af5c6ae
Daniel <danielrs9504@gmail.com>
2021-07-11 18:19:05 -0500

    1. Agrega layout basico de los items de educacion

--- src/components/EducationComponent.js
+++ src/components/EducationComponent.js
@@ -3,8 +3,27 @@ import React from "react";
 const EducationComponent = () => {
   return (
     <div className='col-12'>
-      EducationComponent
-      <div className='progress'>
+      <h1>Education</h1>
+      <div className=' border-bottom'>
+        <h6 className='d-inline border border-primary rounded px-2 text-muted'>
+          2012-2017
+        </h6>
+        <h6 className='d-inline ml-2 text-muted'>University of Antioquia</h6>
+        <h4 className='ml-3 mb-1 text-dark font-weight-bold '>
+          Mechanical Engineer
+        </h4>
+      </div>
+
+      <div className='pt-2 border-bottom'>
+        <h6 className='d-inline border border-primary rounded px-2 text-muted'>
+          2012-2017
+        </h6>
+        <h6 className='d-inline ml-2 text-muted'>University of Antioquia</h6>
+        <h4 className='ml-3 mb-1 text-dark font-weight-bold '>
+          Mechanical Engineer
+        </h4>
+      </div>
+      {/* <div className='progress'>
         <div
           className='progress-bar bg-info'
           role='progressbar'
@@ -13,7 +32,7 @@ const EducationComponent = () => {
           aria-valuemax='100'
           style={{ width: "80%" }}
         ></div>
-      </div>
+      </div> */}
     </div>
   );
 };
