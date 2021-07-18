d077af07893755f5cfa40428b5d15689d6860fa9
Daniel <danielrs9504@gmail.com>
2021-07-18 15:50:17 -0500

    1. Arregla error en el listItem.css nombre 2. Modifica el margin-top del list-item-container

--- src/components/ListItemComponent.js
+++ src/components/ListItemComponent.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../styles/ListItem.css";
+import "../styles/listItem.css";
 
 const EducationListItemComponent = ({ year, title, subtitle, description }) => {
   return (
