22542540b2d2f5d1969c34895d0150a1276b603f
Daniel <danielrs9504@gmail.com>
2021-09-04 05:10:09 -0500

    Modifica el componente ListItemComponent

--- src/components/EducationComponent.js
+++ src/components/EducationComponent.js
@@ -4,60 +4,23 @@ import ListItemComponent from './ListItemComponent';
 const EducationComponent = () => {
   return (
     <div className='col-12 col-lg-8 pt-5'>
-      <div className='row'>
-        <div className='col-auto text-center flex-column d-none d-sm-flex'>
-          <div className='row h-50'>
-            <div className='col'>&nbsp;</div>
-            <div className='col'>&nbsp;</div>
-          </div>
-
-          <div className='m-2'>
-            <i className='fas fa-circle'></i>
-          </div>
-
-          <div className='row h-50'>
-            <div
-              className='col'
-              style={{
-                borderRight: '1px solid blue',
-              }}
-            >
-              &nbsp;
-            </div>
-            <div className='col'>&nbsp;</div>
-          </div>
-        </div>
-
-        <div className='col'>
-          <div className='card'>
-            <div className='card-body'>
-              <h4 className='card-title text-muted mt-0 mb-1 font-weight-bold'>
-                Full-Stack Web Developer
-              </h4>
-              <h4 className='card-title  my-0 '>Make It Real Camp</h4>
-              <div className='text-muted'>
-                <span>2019 - 2020</span>
-                <span
-                  style={{
-                    padding: '0 .45rem 0 0.45rem',
-                  }}
-                >
-                  •
-                </span>
-                <span>Medellin, Colombia</span>
-              </div>
-              <div
-                style={{
-                  padding: '0.75rem 1rem 0.75rem',
-                  color: '#000',
-                }}
-              >
-                Descripcion
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <ListItemComponent
+        bottomLine
+        dotFill
+        title='Full-Stack Web Developer'
+        university='Make It Real Camp'
+        year='2019 - 2020'
+        city='Medellin, Antioquia'
+        description=''
+      />
+      <ListItemComponent
+        topLine
+        title='Mechanical Engineer'
+        university='University of Antioquia'
+        year='2012 - 2017'
+        city='Medellin, Antioquia'
+        description=''
+      />
     </div>
   );
 };
--- src/components/ListItemComponent.js
+++ src/components/ListItemComponent.js
@@ -1,15 +1,104 @@
-import React from "react";
-import "../styles/listItem.css";
+import React from 'react';
+import '../styles/listItem.css';
 
-const EducationListItemComponent = ({ year, company, title, description }) => {
+const ListItemComponent = ({
+  topLine,
+  dotFill,
+  bottomLine,
+  title,
+  university,
+  year,
+  city,
+  description,
+}) => {
   return (
-    <div className='list-item-container'>
-      <h6 className='LI-year'>{year}</h6>
-      <h6 className='LI-company'>{company}</h6>
-      <h4 className='LI-title'>{title}</h4>
-      {description ? <p>{description}</p> : ""}
+    <div className='row'>
+      <div className='col-auto text-center flex-column d-none d-sm-flex'>
+        {/* Primer linea del indicador, si existe la prop topLine no la muestra */}
+        <div className='row h-50'>
+          {topLine ? (
+            <>
+              <div
+                className='col'
+                style={{
+                  borderRight: '1px solid #2196f3',
+                }}
+              >
+                &nbsp;
+              </div>
+              <div className='col'>&nbsp;</div>
+            </>
+          ) : (
+            <>
+              <div className='col'>&nbsp;</div>
+              <div className='col'>&nbsp;</div>
+            </>
+          )}
+        </div>
+
+        <div className='m-2'>
+          {dotFill ? (
+            <i className='fas fa-circle' style={{ color: '#2196f3' }}></i>
+          ) : (
+            <i className='far fa-circle' style={{ color: '#2196f3' }}></i>
+          )}
+        </div>
+
+        <div className='row h-50'>
+          {bottomLine ? (
+            <>
+              <div
+                className='col'
+                style={{
+                  borderRight: '1px solid #2196f3',
+                }}
+              >
+                &nbsp;
+              </div>
+              <div className='col'>&nbsp;</div>
+            </>
+          ) : (
+            <>
+              <div className='col'>&nbsp;</div>
+              <div className='col'>&nbsp;</div>
+            </>
+          )}
+        </div>
+      </div>
+
+      <div className='col mb-4'>
+        <div className='card'>
+          <div className='card-body'>
+            <h4 className='card-title text-muted mt-0 mb-1 font-weight-bold'>
+              {title ? title : <> Title prop</>}
+            </h4>
+            <h4 className='card-title  my-0 '>
+              {university ? university : <>Make It Real Camp</>}
+            </h4>
+            <div className='text-muted'>
+              <span>{year ? year : <>2019 - 2020</>}</span>
+              <span
+                style={{
+                  padding: '0 .45rem 0 0.45rem',
+                }}
+              >
+                •
+              </span>
+              <span>{city ? city : <>Ingresar Ciudad</>}</span>
+            </div>
+            <div
+              style={{
+                padding: '0.75rem 1rem 0.75rem',
+                color: '#000',
+              }}
+            >
+              {description ? description : <>Add Descripcion</>}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default EducationListItemComponent;
+export default ListItemComponent;
