b2094380320ead30e75f407eeadbe815b60bf7b2
Daniel <danielrs9504@gmail.com>
2021-09-05 20:50:55 -0500

    Modificar el breakpoint del card-deck para que se haga responsive en 992, es decir en large Agrega data dummy de la movies app

--- src/screens/PortfolioScreen.js
+++ src/screens/PortfolioScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import mernImg from '../img/mern_homeScreen.png';
+import moviesImg from '../img/togetheater.png';
 
 const PortfolioScreen = () => {
   return (
@@ -9,52 +10,79 @@ const PortfolioScreen = () => {
           <h1 className='mb-3 font-weight-bold' style={{ color: '#2196f3' }}>
             Portfolio
           </h1>
-          <div className='row no-gutters'>
-            <div className='col-lg-6 '>
-              <div className='card border-primary'>
-                <img src={mernImg} className='card-img-top p-3' alt='...' />
-                <div className='card-body '>
-                  <h5 className='card-title'>eCommerce</h5>
-                  <p className='card-text'>
-                    eCommerce application coded with React, Redux, Node, Express
-                    & MondoDB. Some of the build in functionality are:
-                  </p>
-                  <ul>
-                    <li>
-                      Full featured shopping cart with PayPal & credit/debit
-                      payments
-                    </li>
-                    <li>Product rating & review system</li>
-                    <li>Product search, carousel, pagination & more</li>
-                    <li>
-                      Admin interface for product, user & order management
-                    </li>
-                  </ul>
-                  <div className='d-flex justify-content-around'>
-                    <a
-                      href='https://merncommercedny.herokuapp.com/'
-                      className='btn btn-primary'
-                      target='_blank'
-                      rel='noreferrer'
-                    >
-                      Go to website
-                    </a>
-                    <a
-                      href='https://www.linkedin.com/posts/daniel-ramirez-salazar-800081145_mernstack-mernstackdeveloper-javascripts-activity-6775193520903897088-DI71'
-                      className='btn btn-primary'
-                      target='_blank'
-                      rel='noreferrer'
-                    >
-                      Video showcasing the app
-                    </a>
-                  </div>
+
+          <div className='card-deck'>
+            <div className='card border-primary mr-2'>
+              <img src={mernImg} className='card-img-top p-3' alt='...' />
+              <div className='card-body '>
+                <h5 className='card-title'>eCommerce</h5>
+                <p className='card-text'>
+                  eCommerce application coded with React, Redux, Node, Express &
+                  MondoDB. Some of the build in functionality are:
+                </p>
+                <ul>
+                  <li>
+                    Full featured shopping cart with PayPal & credit/debit
+                    payments
+                  </li>
+                  <li>Product rating & review system</li>
+                  <li>Product search, carousel, pagination & more</li>
+                  <li>Admin interface for product, user & order management</li>
+                </ul>
+                <div className='d-flex justify-content-around'>
+                  <a
+                    href='https://merncommercedny.herokuapp.com/'
+                    className='btn btn-primary'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    Go to website
+                  </a>
+                  <a
+                    href='https://www.linkedin.com/posts/daniel-ramirez-salazar-800081145_mernstack-mernstackdeveloper-javascripts-activity-6775193520903897088-DI71'
+                    className='btn btn-primary'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    Video showcasing the app
+                  </a>
                 </div>
               </div>
             </div>
-            <div className='col-lg-6 '>
-              <div className='card ml-2'>
-                <div className='card-body'>
-                  This is some text within a card body.
+            <div className='card border-primary ml-2'>
+              <img src={moviesImg} className='card-img-top p-3' alt='...' />
+              <div className='card-body '>
+                <h5 className='card-title'>eCommerce</h5>
+                <p className='card-text'>
+                  eCommerce application coded with React, Redux, Node, Express &
+                  MondoDB. Some of the build in functionality are:
+                </p>
+                <ul>
+                  <li>
+                    Full featured shopping cart with PayPal & credit/debit
+                    payments
+                  </li>
+                  <li>Product rating & review system</li>
+                  <li>Product search, carousel, pagination & more</li>
+                  <li>Admin interface for product, user & order management</li>
+                </ul>
+                <div className='d-flex justify-content-around'>
+                  <a
+                    href='https://merncommercedny.herokuapp.com/'
+                    className='btn btn-primary'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    Go to website
+                  </a>
+                  <a
+                    href='https://www.linkedin.com/posts/daniel-ramirez-salazar-800081145_mernstack-mernstackdeveloper-javascripts-activity-6775193520903897088-DI71'
+                    className='btn btn-primary'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    Video showcasing the app
+                  </a>
                 </div>
               </div>
             </div>
