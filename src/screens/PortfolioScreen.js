ce4e139b8c032bc67bfc4d7a43ab66328d9815be
Daniel <danielrs9504@gmail.com>
2021-09-12 11:00:46 -0500

    Agrega imagenes al portafolio

--- src/screens/PortfolioScreen.js
+++ src/screens/PortfolioScreen.js
@@ -5,16 +5,22 @@ import moviesImg from '../img/togetheater.png';
 const PortfolioScreen = () => {
   return (
     <div className='container'>
-      <div className='row justify-content-center'>
-        <div className='col-12 col-lg-10 pt-5'>
+      <div className='row justify-content-center '>
+        <div className='col-12 col-lg-12 pt-5'>
           <h1 className='mb-3 font-weight-bold' style={{ color: '#2196f3' }}>
             Portfolio
           </h1>
 
           <div className='card-deck'>
-            <div className='card border-primary mr-2'>
-              <img src={mernImg} className='card-img-top p-3' alt='...' />
-              <div className='card-body '>
+            {/* eCommerce APP */}
+            <div className='card border-primary'>
+              <img
+                src={mernImg}
+                className='card-img-top '
+                alt='eCommerce image'
+                style={{ padding: '1.25rem' }}
+              />
+              <div className='card-body'>
                 <h5 className='card-title'>eCommerce</h5>
                 <p className='card-text'>
                   eCommerce application coded with React, Redux, Node, Express &
@@ -49,39 +55,38 @@ const PortfolioScreen = () => {
                 </div>
               </div>
             </div>
-            <div className='card border-primary ml-2'>
-              <img src={moviesImg} className='card-img-top p-3' alt='...' />
+
+            {/* Movies APP */}
+
+            <div className='card border-primary '>
+              <img
+                src={moviesImg}
+                className='card-img-top '
+                alt='movies app image'
+                style={{ padding: '1.25rem' }}
+              />
               <div className='card-body '>
-                <h5 className='card-title'>eCommerce</h5>
+                <h5 className='card-title'>Togetheater</h5>
                 <p className='card-text'>
-                  eCommerce application coded with React, Redux, Node, Express &
-                  MondoDB. Some of the build in functionality are:
+                  Movies application made with React, Node, Express & MondoDB.
+                  This app fetch data from TMDB API, showing movies that are:{' '}
                 </p>
                 <ul>
-                  <li>
-                    Full featured shopping cart with PayPal & credit/debit
-                    payments
-                  </li>
-                  <li>Product rating & review system</li>
-                  <li>Product search, carousel, pagination & more</li>
-                  <li>Admin interface for product, user & order management</li>
+                  <li>Trending</li>
+                  <li>Now Playing</li>
+                  <li>Popular</li>
+                  <li>Top Rated</li>
+                  <li>Upcoming</li>
+                  <li>Wishlist movies only if the user is registered</li>
                 </ul>
                 <div className='d-flex justify-content-around'>
                   <a
-                    href='https://merncommercedny.herokuapp.com/'
+                    href='https://togetheater.herokuapp.com/'
                     className='btn btn-primary'
                     target='_blank'
                     rel='noreferrer'
                   >
                     Go to website
-                  </a>
-                  <a
-                    href='https://www.linkedin.com/posts/daniel-ramirez-salazar-800081145_mernstack-mernstackdeveloper-javascripts-activity-6775193520903897088-DI71'
-                    className='btn btn-primary'
-                    target='_blank'
-                    rel='noreferrer'
-                  >
-                    Video showcasing the app
                   </a>
                 </div>
               </div>
