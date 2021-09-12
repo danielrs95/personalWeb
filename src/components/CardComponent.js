6ffcd6ee9cc818640fb1ebabd9845836c6a2b61a
Daniel <danielrs9504@gmail.com>
2021-09-12 17:00:57 -0500

    Elimina css no usados de los cards y navbars

--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import '../styles/cardComponent.css';
 import imageCV from '../img/CV.jpg';
 import { Tooltip } from 'bootstrap';
 
--- src/components/ListItemComponent.js
+++ src/components/ListItemComponent.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import '../styles/listItem.css';
 
 const courseList = [
   {
