ccddb6e9e4b46f708254e59e2ca2b3298bfccb49
Daniel <danielrs9504@gmail.com>
2021-09-12 16:41:38 -0500

    Agrega popperjs para poder usar los efectos del tooltip Agrega tooltip a los iconos de las redes sociales

--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../styles/cardComponent.css';
 import imageCV from '../img/CV.jpg';
+import { Tooltip } from 'bootstrap';
 
 const CardComponent = () => {
+  useEffect(() => {
+    //init tooltip
+    Array.from(
+      document.querySelectorAll('a[data-bs-toggle="tooltip"]')
+    ).forEach((tooltipNode) => new Tooltip(tooltipNode));
+  });
+
   return (
     <div className='container mt-4'>
       <div className='row justify-content-center'>
@@ -21,16 +29,24 @@ const CardComponent = () => {
                 <p class='card-text fw-bold fs-1'>Daniel Ramirez</p>
                 <div className='' style={{ fontSize: '4.5rem' }}>
                   <a
+                    role='button'
                     class='fab fa-linkedin link-light text-decoration-none mx-2'
                     href='https://www.linkedin.com/in/daniel-ramirez-salazar-800081145'
                     target='_blank'
                     rel='noreferrer'
+                    data-bs-toggle='tooltip'
+                    data-bs-placement='Bottom'
+                    title='Check my linkedIn account!'
                   ></a>
                   <a
+                    role='button'
                     class='fab fa-github-square link-light text-decoration-none mx-2'
                     href='https://github.com/danielrs95'
                     target='_blank'
                     rel='noreferrer'
+                    data-bs-toggle='tooltip'
+                    data-bs-placement='bottom'
+                    title='Check my GitHub account!'
                   ></a>
                 </div>
               </div>
