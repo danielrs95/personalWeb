8688a42262ca5b30fe10d04597ffd46844408ef6
Daniel <danielrs9504@gmail.com>
2021-09-12 20:09:41 -0500

    Agrega nueva variabla como color primary en el bootstrap y agrega estilos de borda al home card

--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -13,17 +13,34 @@ const CardComponent = () => {
   return (
     <div className='container mt-4'>
       <div className='row justify-content-center'>
-        <div className='col-10'>
-          <div class='card'>
+        <div className='col-11'>
+          <div
+            class='card'
+            style={{
+              border: '0.1px solid rgb(0 0 0 / 10%)',
+              borderRadius: '1.2rem',
+              boxShadow: '5px 5px 1.5px 0 rgb(0 0 0 / 10%)',
+            }}
+          >
             <div
               class='card-header container d-flex bg-gradient justify-content-evenly align-items-center'
               style={{
                 backgroundColor: '#2196f3',
+                borderRadius: '1.2rem 1.2rem 0 0',
               }}
             >
               <div class='row'>
                 <div className='col-12 col-lg-6 d-flex justify-content-center'>
-                  <img src={imageCV} class='img-fluid w-50' alt='...' />
+                  <img
+                    src={imageCV}
+                    class='img-fluid w-50'
+                    alt='...'
+                    style={{
+                      border: '0.1px solid rgb(0 0 0 / 10%)',
+                      borderRadius: '1.2rem',
+                      boxShadow: '5px 5px 1.5px 0 rgb(0 0 0 / 10%)',
+                    }}
+                  />
                 </div>
 
                 <div className='d-flex flex-column col-12 col-lg-6 text-center text-light justify-content-center'>
